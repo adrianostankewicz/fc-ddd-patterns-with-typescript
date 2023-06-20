a9114f16cf5e15eb99355c5766d4f1013cc7824f
Adriano Stankewicz <adriano.stankewicz@gmail.com>
2023-06-20 07:15:17 -0300

    fix: create a value object Address

--- src/entities/customer.ts
+++ src/entities/customer.ts
@@ -1,36 +1,38 @@
+import Address from "./address";
+
 class Customer {
 
   _id: string;
-  _name: string;
-  _address: string;
+  _name: string = "";
+  _address!: Address;
+  _active: boolean = true;
 
-  constructor(id: string, name: string, address: string){
+  constructor(id: string, name: string){
     this._id = id;
     this._name = name;
-    this._address = address;
   }
 
-  get id() : string {
-    return this._id;
+  validate(){
+    if(this._id.length === 0){
+      throw new Error('Id is required');
+    }
+    if(this._name.length === 0){
+      throw new Error('Name is required');
+    }
   }
 
-  get name() : string {
-    return this._name;
+  activate(){
+    if(this._address === undefined){
+      throw new Error("Address is mandatory to activate a customer");
+    }
+    this._active = true;
   }
 
-  get address() : string {
-    return this._address;
-  }
-  
-  set id(id : string) {
-    this._id = id;
-  }
-  
-  set name(name : string) {
-    this._name = name;
+  deactivate(){
+    this._active = false;
   }
 
-  set address(address : string) {
+  setAddress(address: Address){
     this._address = address;
   }
 }
